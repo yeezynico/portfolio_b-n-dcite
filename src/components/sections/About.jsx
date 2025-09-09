37c27fdf0202b2655267d0e43980b25af647cfee
Melinda-EM <esteves.melinda@outlook.fr>
2025-09-09 20:11:50 +0200

    FEAT: About, Service & Process fine, need only style

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -2,30 +2,58 @@ import React from 'react'
 import sunny from '../../assets/img/soleil-orange.png'
 import { Button } from '../ui/Button'
 import photo from '../../assets/img/DymensBeauty - Shoot 2-23 1.png'
+import bgImage from '../../assets/img/quadrillage.png'
+import bgImage2 from '../../assets/img/tache.png'
 
 const About = () => {
   return (
-    <div className="mb-10 border-2">
-        <div className="bg-[url('../../../assets/img/tache.png')]">
-            <div className='bg-[#FEEEDB] rounded-xl flex w-96 h-60'>
-                <h2 className='text-[#AA8F66] font-newzen font-bold text-3xl'>Ensemble donnons vie à votre marque grâce à un univers qui vous ressemble</h2>
-            </div>
-            <div className='font-newzen font-bold text-lg text-[#ED9B40] flex gap-5'>
-                <img src={sunny}/>
+    <div
+    className="h-screen w-full bg-cover bg-center overflow-hidden"
+    style={{ backgroundImage: `url(${bgImage})` }}
+>
+    <div
+        className="w-full h-full bg-cover bg-center flex flex-col items-center justify-center gap-10"
+        style={{ backgroundImage: `url(${bgImage2})` }}
+    >
+        <div className="bg-[#FEEEDB] rounded-3xl w-10/12 md:w-3/4 lg:w-2/3 xl:w-1/2 p-12 shadow-lg mr-96">
+            <h2 className="text-[#AA8F66] font-newzen font-bold text-4xl">
+                Ensemble donnons vie à votre marque grâce à un univers qui vous ressemble
+            </h2>
+        </div>
+
+        <div className="flex items-center justify-center gap-10 text-[#ED9B40] font-newzen font-bold text-lg mr-96">
+            <div className="flex items-center gap-2">
+                <img src={sunny} alt="icone" className="w-6 h-6" />
                 <p>Identité visuelle</p>
-
-                <img src={sunny}/>
+            </div>
+            <div className="flex items-center gap-2">
+                <img src={sunny} alt="icone" className="w-6 h-6" />
                 <p>Support de communication</p>
-
-                <img src={sunny}/>
+            </div>
+            <div className="flex items-center gap-2">
+                <img src={sunny} alt="icone" className="w-6 h-6" />
                 <p>Webdesign</p>
             </div>
-            <Button>Réserver votre appel gratuit</Button>
-            <div className='flex justify-end  h-96'>
-                <img src={photo} alt="photo"/>
+        </div>
+
+        <div className="w-10/12 md:w-3/4 lg:w-2/3 xl:w-1/2 flex flex-col gap-6">
+            <div className="flex justify-center">
+                <Button>
+                    Réserver votre appel gratuit
+                </Button>
             </div>
         </div>
     </div>
+
+    <img 
+    src={photo} 
+    alt="photo" 
+    className="absolute -bottom-30 right-0 h-[110vh] object-cover"
+    />
+
+</div>
+
+
   )
 }
 
--- src/components/sections/Service.jsx
+++ src/components/sections/Service.jsx
@@ -6,25 +6,25 @@ import {Button} from '../ui/Button'
 const Service = () => {
   return (
     <div className='mb-20'>
-        <div className='flex items-center'>
-            <img src={sun} alt="soleil" className='w-14'/>
+        <div className='flex items-center ml-10 mt-10'>
+            <img src={sun} alt="soleil" className='w-14 mr-3 mb-1'/>
             <h1 className='font-newzen font-bold text-[#ED9B40] text-5xl'>Services</h1>
         </div>
-        <div className='grid grid-cols-3 gap-4 justify-center'>
-            <div>
-                <img src={semisun} alt="soleil"/>
-                <h2>Identité de marque</h2>
-                <p>Logos, identités graphiques, chartes graphiques, ...</p>
+        <div className='grid grid-cols-3 items-center gap-10 my-16 mx-18'>
+            <div className='flex flex-col p-14'>
+                <img src={semisun} alt="soleil" className='w-52 mb-4'/>
+                <h2 className='text-[#AA8F66] font-newzen font-bold text-lg mt-3'>Identité de marque</h2>
+                <p className='text-[#ED9B40] font-newzen font-medium mt-3 text-left w-48'>Logos, identités graphiques, chartes graphiques</p>
             </div>
-            <div>
-                <img src={semisun} alt="soleil"/>
-                <h2>Support de communication</h2>
-                <p>Flyer, PLV, affiche, packaging, carte de visite, brochure, dépliant, ...</p>
+            <div className='flex flex-col p-14'>
+                <img src={semisun} alt="soleil" className='w-52 mb-4'/>
+                <h2 className='text-[#AA8F66] font-newzen font-bold text-lg mt-3'>Support de communication</h2>
+                <p className='text-[#ED9B40] font-newzen font-medium mt-3 text-left '>Flyer, PLV, affiche, packaging, carte de visite, brochure, dépliant</p>
             </div>
-            <div>
-                <img src={semisun} alt="soleil"/>
-                <h2>Webdesign</h2>
-                <p>Bannières animés, visuels pour réseaux sociaux, newsletters, emailings, ...</p>
+            <div className='flex flex-col p-14'>
+                <img src={semisun} alt="soleil" className='w-52 mb-4'/>
+                <h2 className='text-[#AA8F66] font-newzen font-bold text-lg mt-3'>Webdesign</h2>
+                <p className='text-[#ED9B40] font-newzen font-medium mt-3 text-left w-48'>Bannières animés, visuels pour réseaux sociaux, newsletters, emailings</p>
             </div>
         </div>
         <div className='flex justify-center'>
